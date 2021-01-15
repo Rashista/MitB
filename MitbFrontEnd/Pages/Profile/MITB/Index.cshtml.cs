98a557504f56962d331375d720267453a73ab83f
Yordan Rashkov <jrashkov@gmail.com>
2021-01-15 16:54:42 +0200

    Create MITB page

---
+++ MitbFrontEnd/Pages/Profile/MITB/Index.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MitbFrontEnd.Pages.Profile.MITB
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
